d66f4fd836a890ece9dd8707ebdf1191a47f7d7f
chris <cseiter@gmail.com>
2021-04-20 07:10:05 -0400

    Button click returns correct fetch results

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
 const proxyURL = "https://cors-anywhere.herokuapp.com/";
 const rootURL = "http://zelda-api.apius.cc/api/";
 const arrTypes = ["Games","Staff","Characters","Monsters","Bosses","Dungeons","Places","Items"];
+document.getElementById("results").addEventListener("click",getGroup)
+
+function getGroup(e) {
+    const selectedGroup = document.getElementById("type-dropdown").value;
+    console.log(selectedGroup);
+//    getResults(selectedGroup,"100").then(console.log)
+}
 
 function populateDropDown (arrTypes) {
     dropContainer = document.getElementById("type-dropdown")
     arrTypes.forEach (dropObj => {
         const dropSelect = document.createElement("option")
         dropSelect.innerText = dropObj;
+        dropSelect.value = dropObj;
         dropContainer.append(dropSelect);
     });
+    console.log(dropContainer);
 }
 
 function getResults(type,limit) {
@@ -18,5 +27,5 @@ function getResults(type,limit) {
         .then(r => r.data)
 }
 
-getResults("games","100").then(console.log);
+//getResults("games","100").then(console.log);
 populateDropDown(arrTypes);
