2118061296e76f05f22ee7ffd58c6596fb39c17f
chris <cseiter@gmail.com>
2021-04-21 18:18:39 -0400

    create table body from test correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
 const proxyURL = "https://cors-anywhere.herokuapp.com/";
 const rootURL = "http://zelda-api.apius.cc/api/";
 const arrTypes = ["Games","Staff","Characters","Monsters","Bosses","Dungeons","Places","Items"];
-const testResults = {
-    "success": true,
-    "count": 2,
-    "data": [
+const testArray = [
         {
             "appearances": [],
             "_id": "5f6e9a9efee1a5347127ca47",
@@ -21,7 +18,16 @@ const testResults = {
             "description": "The Nice Hookshot is an item in A Link Between Worlds",
             "__v": 0
         }
-    ]
+]
+
+const testItem = {
+    "_id": "5f6ce9d805615a85623ec2b7",
+    "name": "The Legend of Zelda",
+    "description": "The Legend of Zelda is the first...",
+    "developer": "Nintendo R&D 4",
+    "publisher": "Nintendo",
+    "released_date": " February 21, 1986",
+    "__v": 0
 }
 
 function populateDropDown (arrTypes) {
@@ -51,7 +57,7 @@ function getResults(type,limit) {
 }
 
 /* <table>
-            <thead>
+            <thead i>
                 <tr id="table-header">
                 </tr>
             </thead>
@@ -59,14 +65,33 @@ function getResults(type,limit) {
             </tbody>
     </table> */
 
-function createGamesTable() {
-    const tableHeaders = ["name","released_date","publisher","developer","description"]
-    const headerRow = document.getElementById("table-header")
-    tableHeaders.forEach(headerObj => {
+function createGamesTableHeaders() {
+    const gamesHeaders = ["name","released_date","publisher","developer","description"]
+    trHeader = document.createElement("tr");
+    gamesHeaders.forEach(headerObj => {
         const th = document.createElement("th")
         th.innerText = headerObj
-        headerRow.appendChild(th)
+        trHeader.append(th)
     });
+    console.log(trHeader);
 }
 
-createGamesTable();
+function createGamesTableBody(itemObj) {
+    const trBody = document.createElement("tr");
+    const tdName = document.createElement("td");
+    const tdDate = document.createElement("td");
+    const tdPub = document.createElement("td");
+    const tdDev = document.createElement("td");
+    const tdDesc = document.createElement("td");
+    tdName.innerText = itemObj.name;
+    tdDate.innerText = itemObj.released_date;
+    tdPub.innerText = itemObj.publisher;
+    tdDev.innerText = itemObj.developer;
+    tdDesc.innerText = itemObj.description;
+    trBody.append(tdName,tdDate,tdPub,tdDev,tdDesc);
+    console.log(trBody);
+}
+
+
+createGamesTableHeaders();
+createGamesTableBody(testItem);
